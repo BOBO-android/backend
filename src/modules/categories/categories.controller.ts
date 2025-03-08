05d733ce68d43a5750ce9b266cef9c749a725d94
lovanbang <lovanbangbox9@gmail.com>
2025-03-08 10:40:38 +0700

    feat: create an api to get all categories

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { Role, Roles } from '@/decorator/roles.decorator';
+import { Public } from '@/decorator/customize';
 
 @Controller('categories')
 export class CategoriesController {
@@ -11,5 +12,11 @@ export class CategoriesController {
   @Roles(Role.Admin)
   async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.createCategory(createCategoryDto);
+  }
+
+  @Get()
+  @Public()
+  async getAllCategories() {
+    return this.categoriesService.getAllCategories();
   }
 }
