45ae8149ad6a1162dc9221770180ef836eb8be57
lovanbang <lovanbangbox9@gmail.com>
2025-03-11 16:12:49 +0700

    feat: create an api to get a food public by slug

--- src/modules/food/controllers/publicFood.controller.ts
+++ src/modules/food/controllers/publicFood.controller.ts
@@ -1,14 +1,14 @@
 import { FoodService } from '../food.service';
-import { Controller, Get, Query, HttpCode } from '@nestjs/common';
+import { Controller, Get, Query, HttpCode, Param } from '@nestjs/common';
 import { Public, ResponseMessage } from '@/decorator/customize';
 
+@Public()
 @Controller('foods')
 export class PublicFoodController {
   constructor(private readonly foodService: FoodService) {}
 
-  @Public()
-  @HttpCode(200)
   @Get('public-offered')
+  @HttpCode(200)
   @ResponseMessage('Get public offered foods successfully')
   getPublicOfferedFoods(
     @Query() query: string,
@@ -16,5 +16,12 @@ export class PublicFoodController {
     @Query('pageSize') pageSize: string,
   ) {
     return this.foodService.getPublicOfferedFoods(query, +current, +pageSize);
+  }
+
+  @Get(':slug')
+  @HttpCode(200)
+  @ResponseMessage('Get public offered foods successfully')
+  getPublicFood(@Param('slug') slug: string) {
+    return this.foodService.findBySlug(slug);
   }
 }
