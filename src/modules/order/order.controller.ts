70fcffe33b215e27b005bd33210c0464d60ba594
lovanbang <lovanbangbox9@gmail.com>
2025-03-13 16:48:44 +0700

    feat: create an api to help user retrive their orders

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body, Request } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Request,
+  HttpCode,
+  Get,
+  Query,
+} from '@nestjs/common';
 import { OrderService } from './order.service';
 import { RequestWithUser } from '@/common/interfaces/request-with-user.interface';
 import { ResponseMessage } from '@/decorator/customize';
@@ -16,5 +24,16 @@ export class OrderController {
   ) {
     const userId = req.user._id;
     return this.orderService.createOrder(userId, createOrderDto.paymentMethod);
+  }
+
+  @Get()
+  @HttpCode(200)
+  @ResponseMessage('Create order successfully')
+  async getOrdersByUser(
+    @Request() req: RequestWithUser,
+    @Query('customerId') customerId: string,
+  ) {
+    const userId = req.user._id;
+    return this.orderService.getOrdersByUser(customerId, userId);
   }
 }
